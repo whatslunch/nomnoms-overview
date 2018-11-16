eea75d9cca4d20dbbc52ae7c3b7e20bd829e2a85
Will Godfrey <w.gowman.godfrey@gmail.com>
2018-11-15 18:05:43 -0800

    Update postgres seed script to function with existing tables

--- database/db1/index.js
+++ database/db1/index.js
@@ -1,24 +1,33 @@
-const mysql = require('mysql');
+const pg = require('pg');
 
 class Restaurant {
   
   constructor() {
-    this.connection = mysql.createConnection({
-      user: 'root',
-      database: 'nomnoms',
-    });
+    this.connectionString = 'postgres://localhost:5432/nomnoms';
   }
 
   getAll(cb) {
-    this.connection.query(`SELECT * FROM restaurants`, cb);
+    pg.connect(this.connectionString, (err1, client, done) => {
+      if(err1) cb(err1, null);
+      const query = client.query(`SELECT * FROM restaurants`);
+      query.on('error', err2 => { cb(err2, null); done(); });
+      query.on('end', data => { cb(null, data); done(); });
+    }
   }
 
   getOne(id, cb) {
-    this.connection.query(`SELECT * FROM restaurants WHERE id = ${id}`, cb);
+    pg.connect(this.connectionString, (err1, client, done) => {
+      if(err1) cb(err1, null);
+      const query = client.query(query(`SELECT * FROM restaurants WHERE id = ${id}`);
+      query.on('error', err2 => { cb(err2, null); done(); });
+      query.on('end', data => { cb(null, data); done(); });
+    }
   }
 
   addOne(opts, cb) {
-    this.connection.query(`INSERT INTO restaurants (${Object.keys(opts).join(', ')}) VALUES(?, ?, ?, ?, ?, ?, ?)`, Object.values(opts), cb);
+    pg.connect(this.connectionString, (err1, client, done) => {
+      if(err1) cb(err1, null);
+    `INSERT INTO restaurants (${Object.keys(opts).join(', ')}) VALUES(?, ?, ?, ?, ?, ?, ?)`, Object.values(opts), cb);
   }
 
   updateOne(id, opts, cb) {
