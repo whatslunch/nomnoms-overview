7f6ba442c0bfe7be53ad33f4e4a5f569c35edce1
Will Godfrey <w.gowman.godfrey@gmail.com>
2018-11-15 16:23:22 -0800

    Seed postgres db with load:db1 script

--- database/db1/seed.js
+++ database/db1/seed.js
@@ -1,13 +1,44 @@
-const promise       = require('bluebird');
 const pg            = require('pg');
-const fs            = promise.promisifyAll(require('fs'));
+const copyFrom      = require('pg-copy-streams').from;
+const fs            = require('fs');
+const path          = require('path')
 const {performance} = require('perf_hooks');
 
 let t0 = performance.now();
 
+pg.connect('postgres://localhost:5432/nomnoms', (err, client, done) => {
+  client.query('DROP TABLE IF EXISTS restaurants')
+    .on('end', () => {
+      client.query('CREATE TABLE IF NOT EXISTS restaurants(id SERIAL PRIMARY KEY, name VARCHAR(100), lat DECIMAL(10, 6), lng DECIMAL(10, 6), address TEXT, cost INT, phone TEXT, website TEXT)');
+      const pgslStream = client.query(copyFrom('COPY restaurants FROM STDIN CSV HEADER'));
+      const filePath = path.join(__dirname, './../seed/restaurants.csv');
+      const fileStream = fs.createReadStream(filePath);
+      fileStream.on('error', done);
+      pgslStream.on('error', done);
+      pgslStream.on('end', () => {
+        let t1 = performance.now();
+        console.log(`elapsed time (restaurants): ${((t1 - t0)/1000).toFixed(2)} seconds`);
+        done();
+      });
+      fileStream.pipe(pgslStream);
+    });
+});
 
+pg.connect('postgres://localhost:5432/nomnoms', (err, client, done) => {
+  client.query('DROP TABLE IF EXISTS images')
+    .on('end', () => {
+      client.query('CREATE TABLE IF NOT EXISTS images(id SERIAL PRIMARY KEY, src VARCHAR(100), restaurant_id INTEGER)');
+      const pgslStream = client.query(copyFrom('COPY images FROM STDIN CSV HEADER'));
+      const filePath = path.join(__dirname, './../seed/images.csv');
+      const fileStream = fs.createReadStream(filePath);
+      fileStream.on('error', done);
+      pgslStream.on('error', done);
+      pgslStream.on('end', () => {
+        let t1 = performance.now();
+        console.log(`elapsed time (images): ${((t1 - t0)/1000).toFixed(2)} seconds`);
+        done();
+      });
+      fileStream.pipe(pgslStream);
+    });
+});
 
-
-
-let t1 = performance.now();
-console.log(`elapsed time: ${t1 - t0}`);
